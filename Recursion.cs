a7a829c26c63f01aaf66c1c4c25baa57e0425dc8
KirbyXcode <kirby_yu@hotmail.com>
2017-08-07 19:47:14 +0800

    Add files via upload

---
+++ Recursion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Recursion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add02(5));
+            Console.ReadLine();
+        }
+
+        //数列1,1,2,3,5,8,13...第n位数是多少?
+        public static int Add(int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            else return Add(n-2)+Add(n-1);
+        }
+
+        public static int Add02(int n)
+        {
+            int[] intArray = new int[n];//声明一个数组存储这个数列
+            for (int i = 0; i < intArray.Length; i++)//用for循环遍历数列中每个数
+            {
+                if (i == 0 || i == 1) intArray[i] = 1;
+                else
+                    intArray[i] = intArray[i - 1] + intArray[i - 2];            
+            }
+            return intArray[n-1];//返回数列中最后一个元素
+        }
+    }
+}
---
+++ TransformHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 变换组件助手类
+/// </summary>
+public class TransformHelper
+{
+    public static Transform GetChild(Transform parentTF, string childName)
+    {
+        //在子物体中根据名称查找
+        Transform childTF = parentTF.FindChild(childName);
+        if (childTF != null) return childTF;
+        for (int i = 0; i < parentTF.childCount; i++)
+        {//将问题转移给子物体
+            childTF = GetChild(parentTF.GetChild(i), childName);
+            if (childTF != null)
+                return childTF;
+        }
+        return null;
+    }
+}
