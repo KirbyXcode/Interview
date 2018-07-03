8e3f60a0fd4db58274c29fbdf52e8b0cc99973b2
KirbyXcode <kirby_yu@hotmail.com>
2018-07-03 10:41:00 +0800

    Add files via upload

---
+++ Log/Scripts/Config/KeyValuesInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SUIFW
+{
+    [Serializable]
+    class KeyValuesInfo
+    {
+        //语言信息
+        public List<KeyValuesNode> ConfigInfo = null;
+    }
+
+    [Serializable]
+    class KeyValuesNode
+    {
+        //键
+        public string Key = null;
+        //值
+        public string Value = null;
+    }
+}
