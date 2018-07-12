23cbf56f19d5316652cabe094ef76cfaf462a5b0
KirbyXcode <kirby_yu@hotmail.com>
2018-07-13 01:10:35 +0800

    Add files via upload

---
+++ SunLight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunLight : MonoBehaviour 
+{
+	IEnumerator Start()
+    {
+        float day = 24.0f;
+        float now = 0.0f;
+        float nightDegree = 210.0f;
+        while(true)
+        {
+            now = (now + Time.deltaTime) % day;
+            float degree = nightDegree + now / day * 360.0f;
+            transform.rotation = Quaternion.Euler(degree, 0, 0);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
